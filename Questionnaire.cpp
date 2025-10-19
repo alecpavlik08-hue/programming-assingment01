e26e87b52e01d8089b9a925fd88a6c622c36b64f
alecpavlik08-hue <alecpavlik08@gmail.com>
2025-10-19 12:47:35 -0400

    Update and rename Questionnaire to Questionnaire.cpp

---
+++ Questionnaire.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str1, str2, str3;
+
+    cout << "Welcome to Al's Questionnaire assignment!!" << endl;
+
+    cout << "Please list your favorite Baseball team: ";
+    cin >> str1;
+    cout << endl;
+
+    cout << "If you have a favorite, please list what conference they are in: "; 
+    cin >> str2;
+    cout << endl;
+
+    cout << "What year did you favorite team last win the world series (if at all): ";
+    cin >> str3;
+    cout << endl;
+
+    cout << "Your favorite team is: " << str1 << endl;
+    cout << "Your favorite team plays in the: " << str2 << endl;
+    cout << "Your favorite team last won the world series back in: " << str3 << endl;
+    
+
+    return 0;
+}
